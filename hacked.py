b545e202b342a05e5df68441be72f2c9f0933567
Absurd Games Dev <104153005+AbsurdGames@users.noreply.github.com>
2022-05-11 15:53:32 +0200

    Add files via upload

---
+++ hacked.py
@@ -0,0 +1,117 @@
+from colorama import Fore
+import time
+import os
+import random
+#IDK what im doing with my life :'(
+#Cool ASCII art:
+print("         .e$$$$e.")
+print("       e$$$$$$$$$$e")
+print("      $$$$$$$$$$$$$$")
+print("     d$$$$$$$$$$$$$$b")
+print("     $$$$$$$$$$$$$$$$")
+print("    4$$$$$$$$$$$$$$$$F")
+print("    4$$$$$$$$$$$$$$$$F")
+print("     $$$' '$$$$' '$$$")
+print("     $$F   4$$F   4$$")
+print("     '$F   4$$F   4$'")
+print("      $$   $$$$   $P")
+print("      4$$$$$'^$$$$$%")
+print("       $$$$F  4$$$$")
+print("        '$$$ee$$$'")
+print("        . *$$$$F4")
+print("         $     .$")
+print("         '$$$$$$'")
+print("          ^$$$$")
+"""print(" 4$$c       ''       .$$r")
+print(" ^$$$b              e$$$'")
+print(" d$$$$$e          z$$$$$b")
+print("4$$$*$$$$$c    .$$$$$*$$$r")
+print(" ''    ^*$$$be$$$*''    ^'")
+print("          '$$$$'")
+print("        .d$$P$$$b")
+print("       d$$P   ^$$$b")
+print("   .ed$$$"      "$$$be.")
+print(" $$$$$$P          *$$$$$$")
+print("4$$$$$P            $$$$$$'")
+print(" '*$$$'            ^$$P")
+print("    ''              ^'")"""
+
+print("[𝘉𝘺 𝘗𝘢𝘶 𝘊𝘢𝘷a]")
+print("")
+i = input("Write 'begin' to start: ")
+
+while i == "":
+    print("That's not a command")
+if i == "begin":
+    time.sleep(1)
+    print("Collecting data...")
+    time.sleep(0.5)
+
+    print("Collecting ip location and converting.")
+
+    print("Hack completed in state of 'succes'.")
+
+    print("Data:")
+    print("IP: 109.223.849")
+    print("Password: 468700vv")
+    time.sleep(5)
+    print("HACKED FINISHED")
+    print("ENCRIPTING AND REMOVING HACKING DATA")
+    time.sleep(5)
+    print("~$COMPLETED")
+
+    os.system('color 0a')
+
+    nums = [0, 1]
+    tm = 0
+
+    while tm < 30:
+        print(random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),random.randrange(1,5)* " ",
+        random.choice(nums),)
+
+        tm = tm + 0.1
+
+        time.sleep(0.01)
+        
+else:
+    print("Exiting the terminal")
+
+print("This program isn't dangerous!")
+print("But don't actually install viruses. Recomendation: install an antivirus!!!")
